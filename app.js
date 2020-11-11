7f1b1ddef7180a810e2fc2b2278645cd33d4217f
Keith <k.shintaku@gmail.com>
2020-11-10 22:41:22 -0800

    carousel and slight restructure

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    // document.getElementsByClassName("side-nav").style.width = "90%";
-    document.getElementById("side-nav").style.width = "72%";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    // document.getElementsByClassName("side-nav").style.width = "90%";
-    document.getElementById("side-nav").style.width = "0";
-}
-
 function toggleNav() {
     if(document.getElementById("nav-icon").className === "") {
         document.getElementById("nav-icon").className = "open"
